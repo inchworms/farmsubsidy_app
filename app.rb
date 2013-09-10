625eb2caf46f912e9ede956d249554723b70e4d2
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-09-10 15:27:12 +0200

    add a recipient varible

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require 'rubygems'
 require 'sinatra'
 require 'sequel'
 
+# connect to an in-memory database
+DB = Sequel.postgres("farmsubsidy_performance")
+
+# connect to the models
+project_root = File.dirname(File.absolute_path(__FILE__))
+Dir.glob(project_root + "/models/*.rb").each{|f| require f}
+
+
 get '/' do
+  @recipient = Recipient.first
   erb :top_payment
 end
