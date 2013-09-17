bf84d498c1ad19f373facacf93e5c765ab7a8410
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-09-17 15:24:49 +0200

    add a new method to model

--- models/paymentyeartotal.rb
+++ models/paymentyeartotal.rb
@@ -29,8 +29,26 @@ class PaymentYearTotal < Sequel::Model
       recipient_name = Recipient.where(id: payment[:recipient_id]).first[:name].gsub("\"", "")
       top_payments_ranked << {rank: index+1, name: recipient_name, amount: payment[:amount_euro].to_i}
     end
-  @json_top_payments_ranked = top_payments_ranked.to_json
+
+    @json_top_payments_ranked = top_payments_ranked.to_json
   end
+
+  def self.sortbypayment
+    limit = 20
+
+    top_payments_sorted_dataset = self.reverse_order(:amount_euro).limit(limit).all
+
+    top_payments_sorted_array = []
+
+    top_payments_sorted_dataset.each_with_index do |payment, index|
+      recipient_name = Recipient.where(id: payment[:recipient_id]).first[:name].gsub("\"", "")
+      year = Year.where(id: payment[:year_id]).first[:year]
+      top_payments_sorted_array << {rank: index+1, name: recipient_name, amount: payment[:amount_euro].to_i, year: year}
+    end
+
+    top_payments_sorted_array.to_json
+  end
+
 end
 
 
