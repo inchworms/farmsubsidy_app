a46659a028890fb97e0fef402c8f3dd104650733
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-09-19 08:58:50 +0200

    outsource javascript for recipient

--- models/recipient.rb
+++ models/recipient.rb
@@ -33,20 +33,9 @@ class Recipient < Sequel::Model
     payments_sorted = self.payments.sort_by { |payment| payment[:year_id] }
     payment_amount_year_array = []
     payments_sorted.each do |payment| 
-      payment_amount_year_array << [ payment.date, format_large_number(payment.amount_euro.to_i)]
+      payment_amount_year_array << [ payment.date, payment.amount_euro.to_i ]
     end
     payment_amount_year_array
   end
-
- # <% payments_sorted = @recipient.payments.sort_by {|payment| payment[:year_id]} %>
- #    <% @payment_amount_array = [] %>
- #    <% payments_sorted.each do |payment| %>
- #      <p><%= format_large_number(payment.amount_euro.to_i) %> Euro in Year <%= payment.date %>.</p>
- #      <% @payment_amount_array << [year, amount] # {amount: payment.amount_euro.to_i, year: Year.year_for(payment.year_id)} %>
- #    <% end %>
- #    <% @year_table.each do |year, amount| %>
- #    # [[2009, '2324'],[2010, '2324,00']]
- #      <p><%= format_large_number(payment.amount_euro.to_i) %> Euro in Year <%= Year.year_for(payment.year_id) %>.</p>
- #    <% end %>
 
 end
