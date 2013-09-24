8a5cbcb5935be21e4765625350a487f51dd8a246
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-09-24 17:54:27 +0200

    remove not needed js files

--- public/js/treemap.js
+++ public/js/treemap.js
@@ -1,71 +1,47 @@
-// console.log(treemap[0].children)
-  // [Object, Object, Object]
 
-  // console.log(treemap[1].parent)
-  // Max
-var mydata = {
-  "name": "Max", "value": 100,
-  "children": [
-  {
-    "name": "Sylvia", "value": 75,
-    "children": [
-    {"name": "Graig", "value": 25},
-    {"name": "Robin", "value": 25},
-    {"name": "Anna", "value": 25}
-    ]
-  },
-  {
-    "name": "David", "value": 75,
-    "children": [
-    {"name": "Jeff", "value": 25},
-    {"name": "Buffy", "value": 25}
-    ]
-  },
-  {
-    "name" : "Mr X", "value": 75,
-    "children": [
-    {"name": "XJeff", "value": 25},
-    {"name": "XBuffy", "value": 25}
-    ]
-  }
-  ]
-};
+var color = d3.scale.category10();
 
-  // var dataset = <%= @payment_total_treemap_array.to_json %>
+// format currency
+var formatNumber = d3.format(",");
 
-// <% p @payment_total_treemap_array %>
-// <% p @payment_total_treemap_array.to_json %>
+// tooltip
+var tip = d3.tip()
+.attr('class', 'd3-tip-treemap')
+.offset([-10, 0])
+.html(function(d) {
+  return d.name + " recieved: " + formatNumber(d.amount_euro) + " Euro";
+})
 
+var canvas = d3.select("#treemap").append("svg")
+  .attr("width", 500)
+  .attr("height", 500);
 
-  var color = d3.scale.category10();
+// initialize tooltip
+canvas.call(tip);
 
-  var canvas = d3.select("#treemap").append("svg")
-    .attr("width", 500)
-    .attr("height", 500);
+var treemap = d3.layout.treemap()
+  .size([500,500])
+  .round(true)
+  .value(function(d) { return d.amount_euro; });
 
-  var treemap = d3.layout.treemap()
-    .size([500,500])
-    .nodes(mydata);
+var root_node = canvas.append("g")
+  .attr("class", "cell")
+  .attr("transform", "translate(0.5,0.5)");
 
-  var cells = canvas.selectAll(".cell")
+d3.json("/treemap.json", function(error, root) {
+  var cells = root_node.datum(root).selectAll(".cell")
     .data(treemap)
-    .enter()
-    .append("g")
-    .attr("class", "cell");
+    .enter();
+
 
   cells.append("rect")
     .attr("x", function(d) { return d.x; })
     .attr("y", function(d) { return d.y; })
     .attr("width", function(d) { return d.dx; })
     .attr("height", function(d) { return d.dy; })
-    .attr("fill", function(d) { return d.children ? null : color(d.parent.name); })
+    .attr("fill", function(d) { return color(d.name); })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide)
     .attr("stroke", "#ffffff");
 
-  cells.append("text")
-    .attr("x", function(d) { return d.x + d.dx/2 })
-    .attr("y", function(d) { return d.y + d.dy/2 })
-    .attr("text-anchor", "middle")
-    .text(function(d) { return d.children ? null : d.name })
-
-
-  console.log(treemap)
+});
