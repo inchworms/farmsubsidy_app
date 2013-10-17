4a70311681b314102e2d258d40f8f936704bc16f
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-10-17 09:00:44 +0200

    add .rspec file

--- spec/year_spec.rb
+++ spec/year_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
-describe "find the year_id for a year" do
-  it { Year.id_for(2004).should eq(1) }
-end
+describe "test the year methods" do
+  it "find the year_id for the year" do
+    expect(Year.id_for(2004)).to eq(1)
+  end
 
-describe "find the year for a year_id" do
-  it { Year.year_for(1).should eq(2004) }
-end
+  it "find the year for a year_id" do
+    expect(Year.year_for(1)).to eq(2004)
+  end
 
-describe "full_range" do
-  it { Year.full_range.should eq(Year.first.year..Year.last.year) }
+  it "full_range" do
+    expect(Year.full_range.should eq(Year.first.year..Year.last.year))
+  end
 end
