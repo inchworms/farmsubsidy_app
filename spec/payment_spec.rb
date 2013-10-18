62a2ceb31e72145cb9800e2ad13ea0cd1e9f401b
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-10-18 08:48:35 +0200

    add the payment test

---
+++ spec/payment_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "test the payment methods" do
+  before(:each) do
+    payment = Payment.new(:year_id => "1")
+  end
+
+  it "get the date for the payment" do
+    expect(payment.date()).to eq(2004)
+  end
+
+end
