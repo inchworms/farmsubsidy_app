693b9ba8d81072076f4c57ead51d3f4f688e610d
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-10-21 21:08:34 +0200

    add a year polpulate method

--- spec/year_spec.rb
+++ spec/year_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe "test the year methods" do
+  before(:all) do
+    DB = Sequel.postgres("farmsubsidy_test")
+    year = DB[:years]
+
+    years_array = [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012]
+    years_array.each do |x|
+      year.insert(
+        year: [x]
+      )
+    end
+  end
+
   it "find the year_id for the year" do
     expect(Year.id_for(2004)).to eq(1)
   end
