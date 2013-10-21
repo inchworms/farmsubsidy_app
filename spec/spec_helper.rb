c875e1c060411f5eedf4169a120b39864c0294e8
Tyranja <tyranja@cassiopeia.uberspace.de>
2013-10-21 20:18:01 +0200

    add a databasecreator class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,21 @@ $LOAD_PATH << "."
 
 require "sequel"
 
+class DatabaseCreator
+  def self.create
+    if system("psql -l | grep farmsubsidy_test")
+      #if it is then drop the db 
+      system("dropdb farmsubsidy_test")
+    end
+    system("createdb farmsubsidy_test")
+  end
+end
+
+DatabaseCreator.create
+
 # connect to an in-memory database
 # TODO: new Database test
-DB = Sequel.postgres("farmsubsidy_development")
+DB = Sequel.postgres("farmsubsidy_test")
 
 require "models/year"
 require "models/payment"
